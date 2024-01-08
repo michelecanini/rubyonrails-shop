95fa287ac7bfe77dda5ac44ff94bc580467861ef
michelecanini <studio.michelecanini@gmail.com>
2024-01-08 15:17:34 +0100

    [FEAT] Add UsersController, ProductsController, ProductTypesController and generate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'product_types/index'
+  get 'product_types/show'
+  get 'product_types/new'
+  get 'product_types/create'
+  get 'product_types/edit'
+  get 'product_types/update'
+  get 'product_types/destroy'
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

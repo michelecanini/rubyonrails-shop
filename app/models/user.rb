a1fcd5a6944c00487b8030ed130f70633b5f1223
michelecanini <studio.michelecanini@gmail.com>
2024-01-10 10:22:41 +0100

    [FEAT] Updated password length to 8 and modified User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :custom_password_validation
+
+  def custom_password_validation
+    if password.present? and not password.match(/\A(?=.*[A-Z])(?=.*[!@#$&*])/)
+      errors.add :password, 'La password deve contenere almeno una lettera maiuscola e un carattere speciale'
+    end
+  end
 end

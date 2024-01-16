e086780aa6bdca008945acafdc995d587aead915
michelecanini <studio.michelecanini@gmail.com>
2024-01-16 09:09:21 +0100

    [FEAT] Add User serializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,12 +20,12 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.dig(:devise_jwt_secret_key)
     jwt.dispatch_requests = [
-      ['POST', %r{^/api/login$}]
+      ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/api/logout$}]
+      ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 1.day.to_i
+    jwt.expiration_time = 30.minutes.to_i
   end
 
   # ==> Controller configuration

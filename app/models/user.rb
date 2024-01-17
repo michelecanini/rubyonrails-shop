a9db1ade96dcdccd4ed69da43d7e2b9cef0300b1
michelecanini <studio.michelecanini@gmail.com>
2024-01-17 23:21:39 +0100

    [FIX] Correct authentication values for username and password in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
+         :jwt_authenticatable, jwt_revocation_strategy: self,
+         :authentication_keys => [:username]
 
   validate :custom_password_validation
 
   def custom_password_validation
-    if password.present? and not password.match(/\A(?=.*[A-Z])(?=.*[!@#$&*])/)
+    if password.present? and not password.match(/\A(?=.*[A-Z])(?=.*[^A-Za-z0-9]).*\z/)
       errors.add :password, 'La password deve contenere almeno una lettera maiuscola e un carattere speciale'
     end
   end
